3f3e3f29186128d4cf82a505e7c34f39d6994eb2
Ta-nya <leeprae@gmail.com>
2021-08-21 19:23:53 +0300

    add test add schema test

--- softwaretesting_examples/test_first.py
+++ softwaretesting_examples/test_first.py
@@ -4,3 +4,23 @@ import requests
 def test_get_locations_for_90210_check_status_code_equals_200():
     response = requests.get("http://api.zippopotam.us/us/90210")
     assert response.status_code == 200
+
+def test_get_locations_for_90210_check_content_type_equals_json():
+    resource = requests.get("http://api.zippopotam.us/us/90210")
+    assert resource.headers['Content-Type'] == "application/json"
+
+def test_get_locations_for_90210_check_country_equals_USA():
+    resource = requests.get("http://api.zippopotam.us/us/90210")
+    resource_body = resource.json()
+    assert resource_body['country'] == "United States"
+
+def test_get_locations_for_us_90210_check_city_equals_beverly_hills():
+    response = requests.get("http://api.zippopotam.us/us/90210")
+    response_body = response.json()
+    assert response_body["places"][0]["place name"] == "Beverly Hills"
+
+def test_get_locations_for_us_90210_check_one_place_is_returned():
+    response = requests.get("http://api.zippopotam.us/us/90210")
+    response_body = response.json()
+    assert len(response_body["places"]) == 1
+
