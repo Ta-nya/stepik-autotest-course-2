891acfa5451fe3fd79ec61c93c74ff7a14c1757f
Ta-nya <leeprae@gmail.com>
2021-08-21 18:25:05 +0300

    refactoring test

--- softwaretesting_examples/test_first.py
+++ softwaretesting_examples/test_first.py
@@ -1,5 +1,6 @@
+import requests
 
- def test_get_locations_for_90210_check_status_code_equals_200():
-     response-requests.get("http://api.zippopotam.us/us/90210")
-     assert response.status_code == 200
 
+def test_get_locations_for_90210_check_status_code_equals_200():
+    response = requests.get("http://api.zippopotam.us/us/90210")
+    assert response.status_code == 200
